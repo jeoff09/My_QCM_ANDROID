0c58e86305ce7d6b9b31b343bc08064d57186043
jeoff09 <jeoffrey.godart@hotmail.fr>
2016-04-20 21:29:31 +0200

    Update Element on menu and Drawable

--- my_qcm/app/src/main/java/com/tactfactory/my_qcm/view/HomeActivity.java
+++ my_qcm/app/src/main/java/com/tactfactory/my_qcm/view/HomeActivity.java
@@ -82,20 +82,17 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.profil_menu) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.rules_menu) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.legal_mentions_menu) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.help_menu) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.logout_menu) {
 
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
