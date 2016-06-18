76742c8ab31d0c83adf24f070aa633a29d4db3a3
jeoff09 <jeoffrey.godart@hotmail.fr>
2016-06-18 14:58:17 +0200

    Update methode to manage Checkbox

--- my_qcm/app/src/main/java/com/tactfactory/my_qcm/data/checkboxListManage/AnswerCheckBoxAdapter.java
+++ my_qcm/app/src/main/java/com/tactfactory/my_qcm/data/checkboxListManage/AnswerCheckBoxAdapter.java
@@ -1,6 +1,7 @@
 package com.tactfactory.my_qcm.data.checkboxListManage;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tactfactory.my_qcm.R;
 import com.tactfactory.my_qcm.entity.Answer;
@@ -22,16 +24,16 @@ import java.util.List;
  */
 public class AnswerCheckBoxAdapter extends ArrayAdapter<Answer> {
 
-    private ArrayList<Answer> answers;
-    private Context context;
+    public ArrayList<Answer> answers;
+    public Context context;
 
-    public AnswerCheckBoxAdapter(ArrayList<Answer> answers,Context context) {
-        super(context, R.layout.row_fragment_list_answer,answers);
+    public AnswerCheckBoxAdapter(ArrayList<Answer> answers, Context context,int ViewResourceId) {
+        super(context,ViewResourceId, answers);
         this.answers = answers;
         this.context = context;
     }
 
-    private static class AnswerHolder{
+    private class AnswerHolder {
 
         public TextView textview_value_answer;
         public CheckBox checkbox_item_answer;
@@ -43,29 +45,42 @@ public class AnswerCheckBoxAdapter extends ArrayAdapter<Answer> {
         View v = convertView;
         AnswerHolder answerHolder = new AnswerHolder();
 
-        if(convertView == null)
-        {
+        if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
-            v = inflater.inflate(R.layout.row_fragment_list_answer,null);
+            v = inflater.inflate(R.layout.row_fragment_list_answer, null);
 
             answerHolder.textview_value_answer = (TextView) v.findViewById(R.id.textview_value_answer);
 
             answerHolder.checkbox_item_answer = (CheckBox) v.findViewById(R.id.checkbox_item_answer);
 
-            answerHolder.checkbox_item_answer.setOnCheckedChangeListener((CompoundButton.OnCheckedChangeListener) context);
+            v.setTag(answerHolder);
 
 
-        }else {
+            answerHolder.checkbox_item_answer.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    CheckBox cb = (CheckBox) v;
+                    Answer answer = (Answer) cb.getTag();
+                    Toast.makeText(getContext().getApplicationContext(),
+                            "Clicked on Checkbox: " + cb.getText() +
+                                    " is " + cb.isChecked(),
+                            Toast.LENGTH_LONG).show();
+                    answer.setSelected(cb.isChecked());
+                }
+            });
+        }
+        else {
             answerHolder = (AnswerHolder) v.getTag();
         }
 
         Answer answer = answers.get(position);
         answerHolder.textview_value_answer.setText(answer.getAns());
         answerHolder.checkbox_item_answer.setChecked(answer.isSelected());
-        answerHolder.checkbox_item_answer.setTag(answer);
 
 
-         return super.getView(position,convertView,parent);
+        return v;
     }
 }
+
+
 
--- my_qcm/app/src/main/java/com/tactfactory/my_qcm/view/questionnaire/ContentQuestionnaireFragment.java
+++ my_qcm/app/src/main/java/com/tactfactory/my_qcm/view/questionnaire/ContentQuestionnaireFragment.java
@@ -8,8 +8,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.tactfactory.my_qcm.R;
+import com.tactfactory.my_qcm.data.checkboxListManage.AnswerCheckBoxAdapter;
 import com.tactfactory.my_qcm.data.checkboxListManage.Answering;
 import com.tactfactory.my_qcm.data.checkboxListManage.CompleteMCQFunctionAdapter;
 import com.tactfactory.my_qcm.entity.Answer;
@@ -17,17 +19,19 @@ import com.tactfactory.my_qcm.entity.Question;
 import com.tactfactory.my_qcm.entity.Result;
 
 import java.util.ArrayList;
+import java.util.zip.Inflater;
 
 /**
  * Created by ProtoConcept GJ on 30/04/2016.
  * Fragment Content list of Answer
  */
-public class ContentQuestionnaireFragment extends Fragment  {
+public class ContentQuestionnaireFragment extends Fragment {
 
     String questionsJson;
     String answersJson;
     String resultJson;
     String answeringJson;
+    AnswerCheckBoxAdapter answerCheckBoxAdapter;
     int questionsPositionList;
     int naviguationValue;
     ListView list_answer;
@@ -45,16 +49,16 @@ public class ContentQuestionnaireFragment extends Fragment  {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_content_questionnaire, container, false);
-        RelativeLayout relative_layout_content = (RelativeLayout)rootView.findViewById(R.id.relative_layout_content);
+        RelativeLayout relative_layout_content = (RelativeLayout) rootView.findViewById(R.id.relative_layout_content);
         //get the class to deserialize element
         completeMCQFunctionAdapter = new CompleteMCQFunctionAdapter();
         // get All Element link with the MCQ
-        questionsJson =  getArguments().getString("list_question");
+        questionsJson = getArguments().getString("list_question");
         questions = completeMCQFunctionAdapter.responseToListQuestion(questionsJson);
-        answersJson =  getArguments().getString("list_answer");
+        answersJson = getArguments().getString("list_answer");
         answers = completeMCQFunctionAdapter.responseToListAnswer(answersJson);
 
-        resultJson =  getArguments().getString("result");
+        resultJson = getArguments().getString("result");
         result = completeMCQFunctionAdapter.responseToResult(resultJson);
 
         answeringJson = getArguments().getString("list_answering");
@@ -66,25 +70,59 @@ public class ContentQuestionnaireFragment extends Fragment  {
         questionShow = completeMCQFunctionAdapter.questionShow(questions, questionsPositionList);
         answersShow = completeMCQFunctionAdapter.answersShow(answers, questionShow);
 
-        list_answer = (ListView)rootView.findViewById(R.id.list_answer);
+        list_answer = (ListView) rootView.findViewById(R.id.list_answer);
 
 
-        if(questionsPositionList == 1)
-        {
+        if (questionsPositionList == 1) {
             System.out.println("Is the first Question");
-            Button previous_question = (Button)relative_layout_content.findViewById(R.id.previous_question);
+            Button previous_question = (Button) relative_layout_content.findViewById(R.id.previous_question);
             previous_question.setEnabled(false);
 
-        }else if (questionsPositionList == questions.size())
-        {
+        } else if (questionsPositionList == questions.size()) {
             System.out.println("Is the last Question");
-            Button next_question = (Button)relative_layout_content.findViewById(R.id.next_question);
+            Button next_question = (Button) relative_layout_content.findViewById(R.id.next_question);
             next_question.setText("Terminer");
-        }
-        else {
+        } else {
             System.out.println("This is not the last or the first question");
         }
 
+        //create an ArrayAdaptar from the String Array
+        answerCheckBoxAdapter = new AnswerCheckBoxAdapter(answers, getContext().getApplicationContext(),
+                R.layout.fragment_content_questionnaire);
+        ListView listView = (ListView) rootView.findViewById(R.id.list_answer);
+        // Assign adapter to ListView
+        listView.setAdapter(answerCheckBoxAdapter);
+
+            checkButtonClick(rootView);
+
         return rootView;
     }
+
+    private void checkButtonClick(View rootView) {
+
+        RelativeLayout relative_layout_content = (RelativeLayout) rootView.findViewById(R.id.relative_layout_content);
+        Button myButton = (Button) relative_layout_content.findViewById(R.id.next_question);
+        myButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                StringBuffer responseText = new StringBuffer();
+                responseText.append("The following were selected...\n");
+                ArrayList<Answer> countryList = answerCheckBoxAdapter.answers;
+                for (int i = 0; i < countryList.size(); i++) {
+                    Answer country = countryList.get(i);
+                    if (country.isSelected()) {
+                        responseText.append("\n" + country.getAns());
+                    }
+                }
+
+                Toast.makeText(getContext().getApplicationContext(),
+                        responseText, Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
 }
+
+
